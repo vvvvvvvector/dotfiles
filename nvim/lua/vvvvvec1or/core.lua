0478348c1602fae7466fb51628226cb2cc27c7dd
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2024-08-04 17:56:50 +0300

    feat(nvim): install package manager and config tabs width

--- nvim/lua/vvvvvec1or/core.lua
+++ nvim/lua/vvvvvec1or/core.lua
@@ -4,6 +4,8 @@ vim.opt.relativenumber = true
 vim.opt.number = true
 
 vim.opt.autoindent = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 
 vim.opt.wrap = false
 
--- nvim/lua/vvvvvec1or/init.lua
+++ nvim/lua/vvvvvec1or/init.lua
@@ -1,4 +1,5 @@
 require('vvvvvec1or.core')
 require('vvvvvec1or.remap')
+require('vvvvvec1or.lazy')
 
 print('hello init.lua')
