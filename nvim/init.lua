1f20fa084c3243a4dd370d4fa1df4aa2e73708d1
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2025-03-12 03:15:21 +0100

    feat(nvim): update config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,50 @@
-require('vvvvvec1or')
+require('config')
+
+vim.keymap.set('n', '<leader>ex', vim.cmd.Ex, { desc = "Open explorer" })
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-f>", "<C-f>zz")
+vim.keymap.set("n", "<C-b>", "<C-b>zz")
+
+vim.keymap.set("n", "<leader>nh", ":nohl<cr>", { desc = "No highlight" })
+vim.keymap.set("n", "<leader>ww", ":set wrap<cr>", { desc = "Set word wrap" })
+vim.keymap.set("n", "<leader>nw", ":set nowrap<cr>", { desc = "Set word no wrap" })
+
+vim.keymap.set("n", "<leader>to", ":tabnew<cr>", { desc = "Open new tab" })
+vim.keymap.set("n", "<leader>tw", ":tabclose<cr>", { desc = "Close current tab" })
+vim.keymap.set("n", "<leader>t]", ":tabn<cr>", { desc = "Go to next tab" })
+vim.keymap.set("n", "<leader>t[", ":tabp<cr>", { desc = "Go to previous tab" })
+
+vim.keymap.set("n", "<leader>x", ":.lua<cr>", { desc = "Run the line under cursor" })
+vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<cr>", { desc = "Run the current file" })
+
+vim.cmd('language en_US')
+
+vim.g.root_spec = { "cwd" }
+vim.g.netrw_banner = 0
+
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.ignorecase = true
+-- vim.opt.clipboard:append('unnamedplus')
+vim.opt.clipboard = 'unnamedplus'
+
+-- Reserve a space in the gutter
+-- This will avoid an annoying layout shift in the screen
+-- vim.opt.signcolumn = 'yes'
+
+vim.api.nvim_set_hl(0, 'HighlightYank', { bg = '#6366f1' })
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank { higroup = 'HighlightYank', timeout = 450 }
+  end,
+})
+
---
+++ nvim/lua/config/plugins/treesitter.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+
+    build = ":TSUpdate",
+
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "c", "lua", "vim", "vimdoc", "query",
+          "typescript", "javascript", "tsx",
+          "sql", "prisma", "json", "jsonc",
+          "markdown", "markdown_inline"
+        },
+
+        auto_install = false,
+
+        highlight = {
+          enable = true,
+
+          disable = function(lang, buf)
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
+          end,
+        },
+      })
+    end,
+  }
+}
