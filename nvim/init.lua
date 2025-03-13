7dc67931547b4c7353ac24baa0063db8ab4bd62f
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2025-03-13 03:41:24 +0100

    feat(nvim): update config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,6 @@
 require('config')
 
-vim.keymap.set('n', '<leader>ex', vim.cmd.Ex, { desc = "Open explorer" })
+vim.keymap.set('n', '<leader>fx', vim.cmd.Ex, { desc = "Open explorer" })
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
