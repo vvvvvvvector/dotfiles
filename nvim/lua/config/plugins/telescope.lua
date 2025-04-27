1238f2fe20cbafbb3257d03bd6b7461bca6f2529
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2025-04-27 21:41:09 +0200

    feat(nvim): change telescope colorscheme

--- nvim/lua/config/plugins/telescope.lua
+++ nvim/lua/config/plugins/telescope.lua
@@ -72,6 +72,32 @@ return {
       vim.keymap.set("n", "<leader>ds", require("telescope.builtin").lsp_document_symbols)
 
       vim.keymap.set("n", "<leader>pb", require("telescope.builtin").buffers)
+
+      local _titles = "#141415"
+      local _files = "#141415"
+
+      local _preview = "#1C1C24"
+      local _prompt = "#252530"
+
+      vim.api.nvim_create_autocmd('VimEnter', {
+        callback = function()
+          -- vim.api.nvim_set_hl(0, "TelescopeMatching", { fg = "#141415", bg = "#141415" })
+
+          vim.api.nvim_set_hl(0, "TelescopeNormal", { fg = _files, bg = _files })
+          vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = _files, bg = _files })
+
+          vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { fg = _preview, bg = _preview })
+          vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { fg = _preview, bg = _preview })
+
+          vim.api.nvim_set_hl(0, "TelescopePreviewTitle", { fg = _titles, bg = "#C48282" })
+
+          vim.api.nvim_set_hl(0, "TelescopePromptNormal", { fg = "#CDCDCD", bg = _prompt })
+          vim.api.nvim_set_hl(0, "TelescopePromptBorder", { fg = _prompt, bg = _prompt })
+
+          vim.api.nvim_set_hl(0, "TelescopePromptTitle", { fg = _titles, bg = "#E8B588" })
+          vim.api.nvim_set_hl(0, "TelescopePromptPrefix", { fg = "#E8B588", bg = _prompt })
+        end
+      })
     end,
   },
 }
--- nvim/lua/config/plugins/theme.lua
+++ nvim/lua/config/plugins/theme.lua
@@ -29,7 +29,6 @@ return {
     config = function()
       -- NOTE: you do not need to call setup if you don't want to.
       require("vague").setup({
-        -- optional configuration here
       })
     end
   }
