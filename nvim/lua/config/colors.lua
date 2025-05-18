9aa66f12663bb9cb9ca6dc36a60cb0d394b54af3
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2025-05-18 02:10:44 +0200

    refactor(nvim): create set_colors function for telescope

--- nvim/lua/config/colors.lua
+++ nvim/lua/config/colors.lua
@@ -2,11 +2,7 @@ local M = {}
 
 local _telescope_reusable = {
   _title_fg = "#141415",
-  _prompt = {
-    _title = {
-      bg = "#e8b589"
-    }
-  }
+  _prompt_title_bg = "#e8b589"
 }
 
 local _telescope = {
@@ -25,12 +21,36 @@ local _telescope = {
     bg = "#252530",
     fg = "#cdcdcd",
     title = {
-      bg = _telescope_reusable._prompt._title.bg,
+      bg = _telescope_reusable._prompt_title_bg,
       fg = _telescope_reusable._title_fg
     },
-    prefix = _telescope_reusable._prompt._title.bg
+    prefix = _telescope_reusable._prompt_title_bg
   }
 }
+
+_telescope.set_colors = function()
+  vim.api.nvim_create_autocmd('VimEnter', {
+    callback = function()
+      vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = _telescope.results.bg })
+      vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = _telescope.results.bg, bg = _telescope.results.bg })
+
+      vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { fg = _telescope.preview.fg, bg = _telescope.preview.bg })
+      vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { fg = _telescope.preview.bg, bg = _telescope.preview.bg })
+      vim.api.nvim_set_hl(0, "TelescopePreviewTitle",
+        { fg = _telescope.preview.title.fg, bg = _telescope.preview.title.bg })
+
+      vim.api.nvim_set_hl(0, "TelescopePromptNormal", { fg = _telescope.prompt.fg, bg = _telescope.prompt.bg })
+      vim.api.nvim_set_hl(0, "TelescopePromptBorder", { fg = _telescope.prompt.bg, bg = _telescope.prompt.bg })
+      vim.api.nvim_set_hl(0, "TelescopePromptTitle",
+        { fg = _telescope.prompt.title.fg, bg = _telescope.prompt.title.bg })
+      vim.api.nvim_set_hl(0, "TelescopePromptPrefix", { fg = _telescope.prompt.prefix })
+
+      -- vim.api.nvim_set_hl(0, "TelescopeMatching", { fg = "#141415", bg = "#141415" })
+
+      -- vim.api.nvim_set_hl(0, "TelescopeSelection", { fg = "green", bg = "red" })
+    end
+  })
+end
 
 local _harpoon = {
   current_file = {
