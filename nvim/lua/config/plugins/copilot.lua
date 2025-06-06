773f9e4f8b62719ab6cc2e508134769e01a88242
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2025-06-06 02:19:21 +0200

    feat(nvim): add copilot.lua plugin

---
+++ nvim/lua/config/plugins/copilot.lua
@@ -0,0 +1,43 @@
+return {
+  "zbirenbaum/copilot.lua",
+
+  config = function()
+    require("copilot").setup({
+      panel = {
+        enabled = false,
+        auto_refresh = false,
+        keymap = {
+          jump_prev = "[[",
+          jump_next = "]]",
+          accept = "<CR>",
+          refresh = "gr",
+          open = "<M-CR>"
+        },
+        layout = {
+          position = "bottom", -- | top | left | right | horizontal | vertical
+          ratio = 0.4
+        },
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = false,
+        hide_during_completion = true,
+        debounce = 75,
+        keymap = {
+          accept = "<M-l>",
+          accept_word = "<C-w>",
+          accept_line = "<C-l>",
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-]>",
+        },
+      },
+      filetypes = {
+        javascript = true,
+        typescript = true,
+        typescriptreact = true,
+        ["*"] = false, -- disable for all other filetypes and ignore default `filetypes`
+      },
+    })
+  end,
+}
