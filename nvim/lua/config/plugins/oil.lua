67152c515a4f25a8b43bca63383db4b152552702
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2025-06-21 13:32:49 +0200

    refactor(nvim): oil preview window max_width

--- nvim/lua/config/plugins/oil.lua
+++ nvim/lua/config/plugins/oil.lua
@@ -11,7 +11,7 @@ return {
       preview_split = "right",
       padding = 0,
       -- max_width and max_height can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
-      max_width = 0.5,
+      max_width = 0.65,
       max_height = 0.9,
     },
     view_options = {
