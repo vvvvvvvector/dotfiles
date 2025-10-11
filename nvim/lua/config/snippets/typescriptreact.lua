41ffbe6ff736a47ce927fbb9ad305bde42ad240c
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2025-10-11 13:04:32 +0200

    feat(nvim): add snippets for function in ts and tsx

--- nvim/lua/config/snippets/typescriptreact.lua
+++ nvim/lua/config/snippets/typescriptreact.lua
@@ -1,6 +1,6 @@
 ---@diagnostic disable: undefined-global
 
-return {
+local snippets = {
   s(
     { trig = "cl (.*)", regTrig = true },
     fmta([[console.log(<>, <>);]], {
@@ -20,5 +20,25 @@ return {
     fmta([[console.dir(<>, { depth: null });]], {
       i(1),
     })
-  )
+  ),
+  s(
+    { trig = "f (.*)", regTrig = true },
+    fmta("function <>(<>: <>) {\n  <> \n};", {
+      f(function(_, s) return s.captures[1] end),
+      i(1),
+      i(2),
+      i(3)
+    })
+  ),
+  s(
+    { trig = "af (.*)", regTrig = true },
+    fmta("async function <>(<>: <>) {\n  <> \n};", {
+      f(function(_, s) return s.captures[1] end),
+      i(1),
+      i(2),
+      i(3)
+    })
+  ),
 }
+
+return snippets
