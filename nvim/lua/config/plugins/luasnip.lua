731f9f2642fc860595b228481947c90e1e163d6c
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2025-10-11 03:18:12 +0200

    feat(nvim): install and config luasnip plugin

---
+++ nvim/lua/config/plugins/luasnip.lua
@@ -0,0 +1,13 @@
+return {
+  "L3MON4D3/LuaSnip",
+
+  config = function()
+    local ls = require("luasnip")
+
+    require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/config/snippets/" })
+
+    vim.keymap.set({ "i" }, "<C-e>", function() ls.expand() end, { silent = true })
+    vim.keymap.set({ "i", "s" }, "<C-j>", function() ls.jump(1) end, { silent = true })
+    vim.keymap.set({ "i", "s" }, "<C-k>", function() ls.jump(-1) end, { silent = true })
+  end
+}
