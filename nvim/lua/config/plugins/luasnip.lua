9bf7a07445701b3cccdab6e1899a85307e9c4676
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2025-10-11 13:04:12 +0200

    refactor(nvim): tweak luasnip keymaps

--- nvim/lua/config/plugins/luasnip.lua
+++ nvim/lua/config/plugins/luasnip.lua
@@ -7,7 +7,7 @@ return {
     require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/config/snippets/" })
 
     vim.keymap.set({ "i" }, "<C-e>", function() ls.expand() end, { silent = true })
-    vim.keymap.set({ "i", "s" }, "<C-j>", function() ls.jump(1) end, { silent = true })
-    vim.keymap.set({ "i", "s" }, "<C-k>", function() ls.jump(-1) end, { silent = true })
+    vim.keymap.set({ "i", "s" }, "<C-k>", function() ls.jump(1) end, { silent = true })
+    vim.keymap.set({ "i", "s" }, "<C-j>", function() ls.jump(-1) end, { silent = true })
   end
 }
