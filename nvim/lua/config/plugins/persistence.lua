42e2fd9829f5d9aac3712eebd3a7ac1592680bb7
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2026-04-24 03:31:47 +0200

    feat(nvim): install persistence plugin

---
+++ nvim/lua/config/plugins/persistence.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "folke/persistence.nvim",
+
+    event = "BufReadPre",
+
+    opts = {},
+
+    keys = {
+      { "<leader>qs", function() require("persistence").load() end,                 desc = "Load session" },
+      { "<leader>qS", function() require("persistence").select() end,               desc = "Select session" },
+      { "<leader>ql", function() require("persistence").load({ last = true }) end,  desc = "Load last session" },
+      { "<leader>qd", function() require("persistence").stop() end,                 desc = "Stop persistence" },
+    },
+  }
+}
