9b2b15ff1b13c2c751781b7f365c81934105741e
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2026-06-14 03:48:13 +0200

    refactor(nvim): tweak render-markdown opts

--- nvim/lua/config/plugins/render-markdown.lua
+++ nvim/lua/config/plugins/render-markdown.lua
@@ -5,5 +5,15 @@ return {
   dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
-  opts = {},
+  opts = {
+    code = {
+      sign = false,
+      width = "block",
+      right_pad = 1,
+    },
+    heading = {
+      sign = false,
+      icons = {},
+    },
+  },
 }
